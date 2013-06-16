41a71d3913476bcc4002386ed533657e79aad4b4
Igor Marques da Silva <igormarquessilva@gmail.com>
2013-06-16 18:38:49 -0300

    mudancas que ainda nao resolveram

--- book.py
+++ book.py
@@ -1,6 +1,10 @@
 from google.appengine.ext import db
 
 class Book(db.Model):
+
+  name = db.StringProperty(required=True)
+  quantity = db.StringProperty(required=True)
+  
   def __init__(self, name, quantity):
       self.name = name
       self.quantity = quantity
